9d9d01a9cf19cec4691dc6c70d3674e9ef5cadf1
Yamini_Gaddeti <154692377+YaaminiG@users.noreply.github.com>
2025-02-05 14:41:27 -0600

    router.js

---
+++ router.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * GET home page
+ */
+router.route('/')
+    .get(function (req, res, next) {
+        res.status(200).send({status:'ok',message:'Welcome user registration api.'});
+    });
+
+/**
+ * Load all routes here
+ */
+require('./app/routers/userRoutes')(router);
+
+
+module.exports = router;
